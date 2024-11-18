8d2a3c713e3682f8a72fc4915e3328adeea6de25
SkyStillBeBlue <1614195673@qq.com>
2024-11-18 15:21:38 +0800

    easy-calculation

---
+++ cpp/class practice/1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+double a[3][5];
+int main(){
+    double *p1=a[1];
+}
---
+++ cpp/easy-calculation/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double celsius, fahrenheit;
+
+    cout << "Enter a Celsius temperature: ";
+    cin >> celsius;
+
+    fahrenheit = 9.0 / 5 * celsius + 32;
+    cout << "Fahrenheit equivalent: " << fahrenheit << endl;
+
+    return 0;
+}
