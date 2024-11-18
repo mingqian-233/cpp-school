c4d8ca75d2dc00835fdc1bf040f1068977454e24
SkyStillBeBlue <1614195673@qq.com>
2024-11-18 16:09:26 +0800

    loop structure

---
+++ cpp/Homework/Loop-structure/1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x, y;
+    cin >> x;
+    cin >> y;
+    int i = 1;
+    int power = 1;
+    power *= x;
+    i++;
+    if (x <= y) {
+    }
+    cout << power;
+    return 0;
+}
